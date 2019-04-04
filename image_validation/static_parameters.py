63c86d6f4ca6c02a823e8220d3e9ab1b7569846f
Paolo Cozzi <bunop@libero.it>
2019-04-04 14:02:52 +0200

    Upgrading ruleset to v1.5

--- image_validation/static_parameters.py
+++ image_validation/static_parameters.py
@@ -6,6 +6,6 @@ from . import use_ontology
 
 ruleset_filename = os.path.join(
     os.path.dirname(use_ontology.__file__),
-    "sample_ruleset_v1.4.json")
+    "sample_ruleset_v1.5.json")
 
 ontology_library = use_ontology.OntologyCache()
