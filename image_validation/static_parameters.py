2df92ca30546e8501afdc02686206979b9c4db61
Paolo Cozzi <bunop@libero.it>
2019-10-03 11:07:08 +0200

    update ruleset with new sample storage

--- image_validation/static_parameters.py
+++ image_validation/static_parameters.py
@@ -8,7 +8,7 @@ from . import use_ontology
 # ruleset file
 ruleset_filename = os.path.join(
     os.path.dirname(use_ontology.__file__),
-    "sample_ruleset_v1.5.json")
+    "sample_ruleset_v3a0ee76.json")
 
 # ontology cache
 ontology_library = use_ontology.OntologyCache()
